ca97547b346e335afaeed85f1157efb378d4c876
zainhasan <zain.hasan@kravemart.com>
2023-01-25 00:59:11 +0500

    added ValidationPipe

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 const bodyParser = require('body-parser');
@@ -6,6 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(bodyParser({ limit: '524288000' }));
   app.enableCors();
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
