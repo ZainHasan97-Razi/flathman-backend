d55d86659eaf89bf1647bb4850f16cdabf27c2b4
Zain Hasan <zain.hasan@kravemart.com>
2025-03-24 17:15:00 +0500

    indexes compound and unique

--- src/statsConfig/statsConfig.model.ts
+++ src/statsConfig/statsConfig.model.ts
@@ -2,11 +2,13 @@ import * as mongoose from 'mongoose';
 
 export const StatsConfigSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true, unique: true },
-    slug: { type: String, required: true, unique: true },
+    name: { type: String, required: true },
+    slug: { type: String, required: true},
     description: { type: String, required: true },
     parentId: { type: mongoose.Types.ObjectId, default: null },
     parentSlug: { type: String, default: null },
   },
   { timestamps: true },
 );
+
+StatsConfigSchema.index({ name: 1, slug: 1, parentId: 1, parentSlug: 1 }, { unique: true });
