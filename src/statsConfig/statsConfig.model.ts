0bad877ac9b9f6cf5f5542371d696243813b957f
Zain Hasan <zain.hasan@kravemart.com>
2025-03-24 15:17:57 +0500

    indexes

--- src/statsConfig/statsConfig.model.ts
+++ src/statsConfig/statsConfig.model.ts
@@ -2,8 +2,8 @@ import * as mongoose from 'mongoose';
 
 export const StatsConfigSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true },
-    slug: { type: String, required: true },
+    name: { type: String, required: true, unique: true },
+    slug: { type: String, required: true, unique: true },
     description: { type: String, required: true },
     parentId: { type: mongoose.Types.ObjectId, default: null },
     parentSlug: { type: String, default: null },
