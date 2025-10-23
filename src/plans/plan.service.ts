3706e75067858714fde0863ed9188ce2d9032574
Zain Hasan <zain.hasan@kravemart.com>
2025-10-23 13:44:35 +0500

    Update plan.service.ts

--- src/plans/plan.service.ts
+++ src/plans/plan.service.ts
@@ -2,30 +2,40 @@ import {
   Injectable,
   NotFoundException,
   BadRequestException,
-  InternalServerErrorException,
-  ConflictException,
 } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-// import { TEAM_SCHEMA_TYPE } from './team.model';
-// import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { Plan } from './plan.model';
+import { Plan, PlanDocument, PlanStatusEnum, PlanTypeEnum } from './plan.model';
 import { CreatePlanDto } from './dto/create.plan.dto';
 import { UpdatePlanDto } from './dto/update.plan.dto';
 import { MongoIdType } from 'src/common/common.types';
-// import { CreatePlayerDto } from './dto/create.player.dto';
-// import { UpdatePlayerDto } from './dto/update.player.dto';
-// import { CreateTeamDto } from 'src/team/dto/create.team.dto';
-// import { TeamService } from 'src/team/teams.services';
 
 @Injectable()
 export class PlanService {
   constructor(
     @InjectModel('Plan') private planModel: Model<Plan>,
-  ) {}
+  ) { }
+  
+  getModel() {
+    return this.planModel;
+  }
 
   async create(body: CreatePlanDto) {
     try {
+      const activePlans: PlanDocument[] = await this.planModel.find({ status: PlanStatusEnum.active });
+      if (body.type === PlanTypeEnum.annual) {
+        /*If annual plan then it's created as default drafted status, incase if we are creating it with active status 
+        then we must put validation here to check that there are no simultaneous active annual plans*/
+      } else {
+        let hasOverlappingMonth = false;
+        activePlans.forEach(plan => {
+          const hasCommonMonth = plan.months.some(month => body.months.includes(month))
+          if(hasCommonMonth) hasOverlappingMonth = true;
+        });
+        if (hasOverlappingMonth) {
+          throw new BadRequestException('Overlapping month found');
+        }
+      }
       return await this.planModel.create(body);
     } catch (e) {
       throw e;
@@ -37,6 +47,24 @@ export class PlanService {
       const plan = await this.planModel.findById(_id);
       if (!plan) {
         throw new NotFoundException('Plan not found');
+      }
+
+      const activePlans: PlanDocument[] = await this.planModel.find({ status: PlanStatusEnum.active });
+      if (plan.type === PlanTypeEnum.annual) {
+        if (body.status && body.status === PlanStatusEnum.active) {
+          if(activePlans.find(p => p.status === PlanStatusEnum.active)) throw new BadRequestException('Active annual plan already exists');
+        }
+      } else {
+        if (body.months && body.months.length > 0) {
+          let hasOverlappingMonth = false;
+          activePlans.forEach(plan => {
+            const hasCommonMonth = plan.months.some(month => body.months.includes(month))
+            if(hasCommonMonth) hasOverlappingMonth = true;
+          });
+          if (hasOverlappingMonth) {
+            throw new BadRequestException('Overlapping month found');
+          }
+        }
       }
       Object.assign(plan, body);
       return await plan.save();
